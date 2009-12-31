faa11448fe42a2752ed12895ac76842b0d480c04
Mikko Matilainen <mikkom@iki.fi>
2009-12-31 11:13:11 +0200

    - Code cleanup.

--- nordea2qif.py
+++ nordea2qif.py
@@ -10,6 +10,10 @@ import htmllib
 import re
 import sys
 
+class TransactionType:
+    UNKNOWN = 0
+    BANK = 1
+
 def unescape(s):
     p = htmllib.HTMLParser(None)
     p.save_bgn()
@@ -19,10 +23,10 @@ def unescape(s):
 def print_qif(transaction_type, transaction_date, total, payee, memo = None):
     # Type
     qif = '!Type:'
-    if transaction_type != 666:
+    if transaction_type == TransactionType.BANK:
         qif += 'Bank'
     else:
-        qif += 'DevilsDue'
+        raise Exception('Unsupported transaction type.')
     qif += '\n'
     # Date
     qif += 'D' + transaction_date.strftime("%m/%d/%Y") + '\n'
@@ -36,12 +40,24 @@ def print_qif(transaction_type, transaction_date, total, payee, memo = None):
     qif += '^\n'
     return qif
 
+#
 # Main
-if len(sys.argv) != 2:
+#
+
+ARGUMENT_COUNT = 2
+FILENAME_INDEX = 1
+
+DATE_INDEX = 0
+TOTAL_INDEX = 3
+PAYEE_INDEX = 4
+MEMO_INDEX = 10
+MIN_FIELD_COUNT = PAYEE_INDEX + 1
+
+if len(sys.argv) != ARGUMENT_COUNT:
     print 'Usage: nordea2qif.py [file]'
     sys.exit()
 
-filename = sys.argv[1]
+filename = sys.argv[FILENAME_INDEX]
 f = open(filename, 'rb')
 content = unicode(f.read(), 'iso-8859-1')
 f.close()
@@ -53,30 +69,30 @@ output = ''
 
 lines = str.splitlines()
 for line in lines:
-    if re.match('\d', line) != None:
+    if re.match('\d', line) == None:
+        print 'Line does not start with a number, skipping...'
+    else:
         fields = line.split('\t')
-        if len(fields) < 5:
+        if len(fields) < MIN_FIELD_COUNT:
             print 'Not enough fields in the line, skipping...'
         else:
-            transaction_date = datetime.strptime(fields[0], '%d.%m.%Y')
+            transaction_date = datetime.strptime(fields[DATE_INDEX], '%d.%m.%Y')
 
-            totalstr = fields[3]
+            totalstr = fields[TOTAL_INDEX]
             totalstr = totalstr.replace('.', '')
             totalstr = totalstr.replace(',', '.')
             total = float(totalstr)
 
-            payee = unescape(fields[4])
+            payee = unescape(fields[PAYEE_INDEX])
 
             memo = None
-            if len(fields) >= 11 and fields[10] != '':
-                memo = unescape(fields[10])
+            if len(fields) > MEMO_INDEX and fields[MEMO_INDEX] != '':
+                memo = unescape(fields[MEMO_INDEX])
 
-            transaction_type = 1 # Fixed, for now
+            transaction_type = TransactionType.BANK  # Hardcoded, for now
 
             output += print_qif(transaction_type, transaction_date, total, payee, memo)
-    else:
-        print 'Line does not start with a number, skipping...'
 
-f = open(filename + '.qif', 'w')
+f = open('Checking Account.qif', 'w')
 f.write(output)
 f.close()
